3498a2f7153f4590bb825858e2efcc659e4db89b
Michael Dance <michael.dance@gmail.com>
2025-04-05 13:56:30 +1300

    Refactored ph formula

--- src/ph4502c_sensor.cpp
+++ src/ph4502c_sensor.cpp
@@ -13,27 +13,26 @@ void PH4502C_Sensor::recalibrate(float calibration) {
 }
 
 float PH4502C_Sensor::read_ph_level() {
-    float reading = 0.0f;
+    float total = 0.0f;
 
-    for(int i = 0; i < this->_reading_count; i++) {
-        reading += analogRead(this->_ph_level_pin);
-        delayMicroseconds(this->_reading_interval);
+    for (int i = 0; i < this->_reading_count; i++) {
+        total += analogRead(this->_ph_level_pin);
+        delay(this->_reading_interval);
     }
 
-    reading = PH4502C_VOLTAGE / this->_adc_resolution * reading;
-    reading /= this->_reading_count;
-    reading = this->_calibration + ((PH4502C_MID_VOLTAGE - reading)) / PH4502C_PH_VOLTAGE_PER_PH;
+    float avg_adc = total / this->_reading_count;
+    float voltage = (avg_adc / this->_adc_resolution) * PH4502C_VOLTAGE;
+    
+    float ph = this->_calibration + (PH4502C_MID_VOLTAGE - voltage) / PH4502C_PH_VOLTAGE_PER_PH;
 
-    return reading;
+    return ph;
 }
 
 float PH4502C_Sensor::read_ph_level_single() {
-    float reading = analogRead(this->_ph_level_pin);
+    float adc = analogRead(this->_ph_level_pin);
+    float voltage = (adc / this->_adc_resolution) * PH4502C_VOLTAGE;
 
-    reading = PH4502C_VOLTAGE / this->_adc_resolution * reading;
-    reading /= this->_reading_count;
-
-    return this->_calibration + ((PH4502C_MID_VOLTAGE - reading)) / PH4502C_PH_VOLTAGE_PER_PH;
+    return this->_calibration + (PH4502C_MID_VOLTAGE - voltage) / PH4502C_PH_VOLTAGE_PER_PH;
 }
 
 int PH4502C_Sensor::read_temp() {
@@ -43,3 +42,4 @@ int PH4502C_Sensor::read_temp() {
 
     return analogRead(this->_temp_pin);
 }
+
